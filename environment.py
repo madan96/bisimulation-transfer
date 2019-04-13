Minor fix in transition calculation

environment.py
@@ -71,15 +71,16 @@ class Environment(object):
             # if state in blocked_ids:
             # 	continue
             for action in range(self.action_space):
-                num_valid_next_states = 1 + sum([int(self.check_possible_state(state)) for state in [(pos[0]+1, pos[1]), (pos[0]-1, pos[1]), (pos[0], pos[1]+1), (pos[0], pos[1]-1)]])
+                num_valid_next_states = sum([int(self.check_possible_state(state)) for state in [(pos[0]+1, pos[1]), (pos[0]-1, pos[1]), (pos[0], pos[1]+1), (pos[0], pos[1]-1)]])
+                print  ("num: ", num_valid_next_states)
                 total_assigned_prob = 0
-                proposed = (pos[0] +1, pos[1])
+                proposed = (pos[0] + 1, pos[1])
                 if self.check_possible_state(proposed):
                     propose_idx = self.state2idx[proposed]
                     if action == 0:
                         tp_matrix[state][action][propose_idx] = 0.9
                     else:
-                        tp_matrix[state][action][propose_idx] = 0.1/(num_valid_next_states-1) if num_valid_next_states>1 else 0.1
+                        tp_matrix[state][action][propose_idx] = 0.1 / num_valid_next_states if num_valid_next_states > 1 else 0.1
                     total_assigned_prob += tp_matrix[state][action][propose_idx]
                     
                 proposed = (pos[0] - 1, pos[1])
@@ -88,7 +89,7 @@ class Environment(object):
                     if action == 1:
                         tp_matrix[state][action][propose_idx] = 0.9
                     else:
-                        tp_matrix[state][action][propose_idx] = 0.1/(num_valid_next_states-1) if num_valid_next_states>1 else 0.1
+                        tp_matrix[state][action][propose_idx] = 0.1 / num_valid_next_states if num_valid_next_states > 1 else 0.1
                     total_assigned_prob += tp_matrix[state][action][propose_idx]
                     
                 proposed = (pos[0], pos[1] + 1)
@@ -97,7 +98,7 @@ class Environment(object):
                     if action == 2:
                         tp_matrix[state][action][propose_idx] = 0.9
                     else:
-                        tp_matrix[state][action][propose_idx] = 0.1/(num_valid_next_states-1) if num_valid_next_states>1 else 0.1
+                        tp_matrix[state][action][propose_idx] = 0.1 / num_valid_next_states if num_valid_next_states > 1 else 0.1
                     total_assigned_prob += tp_matrix[state][action][propose_idx]
                     
                 proposed = (pos[0], pos[1] - 1)	
@@ -106,10 +107,10 @@ class Environment(object):
                     if action == 3:
                         tp_matrix[state][action][propose_idx] = 0.9
                     else:
-                        tp_matrix[state][action][propose_idx] = 0.1/(num_valid_next_states-1) if num_valid_next_states>1 else 0.1
+                        tp_matrix[state][action][propose_idx] = 0.1 / num_valid_next_states if num_valid_next_states > 1 else 0.1
                     total_assigned_prob += tp_matrix[state][action][propose_idx]
-                tp_matrix[state][action][state] = 1.0 - total_assigned_prob
 
+                tp_matrix[state][action][state] = 1.0 - total_assigned_prob
                     
         # tp_matrix = np.delete(tp_matrix, blocked_ids, axis=0)
         # tp_matrix = np.delete(tp_matrix, blocked_ids, axis=2)